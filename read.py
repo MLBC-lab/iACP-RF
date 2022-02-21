cf05741fcc4c7cb0bd39e9425ee5e9c029e47221
MehediAzim <mehediazimpiash@gmail.com>
2022-02-22 00:28:52 +0600

    Reading fasta

---
+++ read.py
@@ -0,0 +1,45 @@
+from sklearn.preprocessing import LabelEncoder
+
+def readFASTAs(fileName):
+
+    '''
+    :param fileName:
+    :return: peptide sequences
+    '''
+    with open(fileName, 'r') as file:
+        v = []
+        genome = ''
+        for line in file:
+            if line[0] != '>':
+                genome += line.strip()
+            else:
+                v.append(genome.upper())
+                genome = ''
+        v.append(genome.upper())
+        del v[0]
+        return v
+
+def readLabels(fileName):
+
+    '''
+    :param fileName:
+    :return: label of peptide sequences
+    '''
+    with open(fileName, 'r') as file:
+        v = []
+        for line in file:
+            if line != '\n':
+                v.append((line.replace('\n', '')).replace(' ', ''))
+        return v
+
+
+
+def fetchXY(FASTAs, Labels):
+    X = readFASTAs(FASTAs)
+    Y = readLabels(Labels)
+
+    Y = LabelEncoder().fit_transform(Y)
+
+    assert len(X)==len(Y), 'Numbers of sequence and number of labels are not equal.'
+
+    return X, Y
